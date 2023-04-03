d6c16f3e515291937f2165f8e66ced7773ac8712
kimkisic <rlarltlr123@naver.com>
2023-04-03 18:00:05 +0900

    merge

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,42 +1,6 @@
 import axios from "axios";
 
-// const localtoken = localStorage.token;
-
-export const fetchCreate = (url: string, data: string) => {
-  fetch(url, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  }).catch((error) => {
-    console.error("Error", error);
-  });
-};
-export const fetchPatch = (url: string, id: string, data: string) => {
-  fetch(`${url}${id}`, {
-    method: "PATCH",
-    headers: { "Content-Type": "Application/json" },
-    body: JSON.stringify(data),
-  })
-    .then(() => {
-      window.location.reload();
-    })
-    .catch((error) => {
-      console.error("Error", error);
-    });
-};
-export const fetchPut = (url: string, id: string, data: string) => {
-  fetch(`${url}${id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "Application/json" },
-    body: JSON.stringify(data),
-  })
-    .then(() => {
-      window.location.reload();
-    })
-    .catch((error) => {
-      console.error("Error", error);
-    });
-};
+const localtoken = localStorage.token;
 
 const BASE_URL = "http://3.39.150.26:8080";
 // 기본 헤더없는 요청
@@ -46,10 +10,10 @@ const axiosApi = (url: string) => {
 };
 // 토큰 인증 요청
 const axiosAuthApi = (url: string) => {
-  // const token = localtoken;
+  const token = localStorage.token;
   const instance = axios.create({
     baseURL: url,
-    // headers: { Authorization: "Bearer " + token },
+    headers: { Authorization: token },
   });
   return instance;
 };
